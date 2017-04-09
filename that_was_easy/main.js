c905e2ab967de3a85259fa54d907417b5cbc4e53
Brian Wu <bwubrian@gmail.com>
2017-04-09 07:21:49 +0000

    space press button

--- that_was_easy/main.js
+++ that_was_easy/main.js
@@ -1,3 +1,5 @@
+/*global $*/
+
 $("#easy").on("click",sayThatWasEasy);
 
 
@@ -5,6 +7,14 @@ $("#easy").on("click",sayThatWasEasy);
 function sayThatWasEasy(){
     var thatWasEasy = new Audio("that_was_easy.mp3");
     thatWasEasy.play();
+}
+
+$(document).keypress(makeThisHappen);
+
+function makeThisHappen(event){
+    if(event.charCode == 32){
+        $("#easy").trigger("click");
+    }
 }
 
 
