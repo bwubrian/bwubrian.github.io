b9ae00f817d6761eed3be84cfeccfa7fb0aadf4a
Brian Wu <bwubrian@gmail.com>
2017-04-10 02:26:33 +0000

    made truth prettier

--- truth/script.js
+++ truth/script.js
@@ -1,5 +1,19 @@
-document.getElementById("go").onclick = function(){
+$("#go").on("click",addTruth);
+
+
+function addTruth(){
     var inputText = $("#text_input").val();
     var truth = inputText+" is not as awesome as Brian Wu";
     document.getElementById("output").innerHTML = truth;
+}
+
+$(document).keypress(makeThisHappen);
+
+function makeThisHappen(event){
+    
+    if(event.charCode == 13){
+        $("#go").trigger("click");
+    }
+    
+    //("#go").trigger("click");
 }
